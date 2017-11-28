34c758d2b8c265567b2bd7dd1fad0c06065d7c1e
MyMac <mymac@MyMacs-MacBook-Pro.local>
2017-11-28 09:05:15 -0600

    bm.style

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-let menuHTML = '<div id="nav-menu-title" class="nav-menu-title"><h1>Noncomtroph\'s</h1></div><ul><li><a href="#">Home</a></li><li><a href="#">Products</a></li><li><a href="#">Careers</a></li><li><a href="#">About</a></li><li><a href="#">We Tried</a></li></ul>';
+let menuHTML = '<div id="nav-menu-title" class="nav-menu-title"><h1>NonComTroph\'s</h1></div><ul><li><a href="#">Home</a></li><li><a href="#">Products</a></li><li><a href="#">Careers</a></li><li><a href="#">About</a></li><li><a href="#">We Tried</a></li></ul>';
 
 
 document.getElementById("nav-menu").innerHTML = menuHTML;
